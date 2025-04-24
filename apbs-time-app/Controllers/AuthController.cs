0b0abad33ce75fbb4ec52fa3763093396089e2d5
youssef-khe <168106447+youssef-khe@users.noreply.github.com>
2025-04-24 10:33:23 +0100

    adderd return user id on login

--- apbs-time-app/Controllers/AuthController.cs
+++ apbs-time-app/Controllers/AuthController.cs
@@ -35,7 +35,8 @@ public class AuthController : ControllerBase
         return Ok(new
         {
             authToken = authToken.Result,
-            accessToken
+            accessToken,
+            user.Id
         });
     }
     [Authorize]
