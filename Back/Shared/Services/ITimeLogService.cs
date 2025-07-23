80a4c22a6ce2a4b180fa1e367dc8afa55619fdd3
KETATA Ahmed <140930132+845M@users.noreply.github.com>
2025-07-23 15:37:16 +0100

    made function to calculate total hours worked per day and per week and made to be shown in the dashboard (for the total hours perday it will be normal 00h 00m and for the function per week in style of graph each day how many hours) (#36)

--- Back/Shared/Services/ITimeLogService.cs
+++ Back/Shared/Services/ITimeLogService.cs
@@ -10,7 +10,9 @@ namespace Shared.Services
         Result StartTracking(Guid accountId);
         Result PauseTracking(Guid accountId);
         Result StopTracking(Guid accountId);
-        List<TimeLog> GetLogs(Guid accountId);
+        List<TimeLog> GetLogs(Guid accountId); // Fixed return type
         Result UpdateLog(Guid logId, DateTime newTime, TimeLogType newType, int? totalHours = null);
+        string GetTodayTotalHours(Guid id); // Changed to string
+        Dictionary<string, double> GetWeeklyHours(Guid id);
     }
 }
